a3cb3f22da7983efe29ef6213ea13b17d7b47564
Denperidge <cat@denperidge.com>
2026-01-02 14:33:43 +0100

    Deno in nix

--- shell.nix
+++ shell.nix
@@ -2,5 +2,10 @@
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
       pnpm
+      steam-run-free
     ];
+
+    shellHook = ''
+      alias deno="steam-run ./node_modules/deno/deno"
+    '';
 }
