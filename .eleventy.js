fc8f6f3b861d3252e63f72763d1b96265333653d
Denperidge <cat@denperidge.com>
2026-01-02 18:22:23 +0100

    Implement pug

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
+import pugPlugin from "@11ty/eleventy-plugin-pug";
 
 export default async function(eleventyConfig) {
+    eleventyConfig.addPlugin(pugPlugin);
     return {
         dir: {
             input: "src",
