539b5a64b282c3fedcbfe51f8ba0391cb727b41b
Denperidge <cat@denperidge.com>
2026-01-03 21:51:37 +0100

    First draft table gen & not implemented

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,11 +1,33 @@
+function getToolIds(data) {
+    return data.packageManagers.tools.map(tool => tool.id);
+}
+
 export default {
     tools: (data) => {
         return data.packageManagers.tools;
     },
-    toolKeys: (data) => {
-        return Object.keys(data.packageManagers.tools);
+    toolIds: (data) => {
+        return getToolIds(data);
     },
     features: (data) => {
-        return data.packageManagers.features;
+        const toolIds = getToolIds(data)
+        return data.packageManagers.features.map(feature => {
+            console.log(feature)
+            for (let i = 0; i < toolIds.length; i++) {
+                const toolId = toolIds[i];
+                console.log(toolId)
+                const implementation = feature.implementations[toolId];
+                
+                let prettyPrint = "";                
+                if (implementation == undefined) {
+                    feature.implementations[toolId] = {
+                        prettyPrint: "❌ Not implemented"
+                    }
+                } else if (implementation.warning) {
+                    feature.implementations[toolId].prettyPrint += "⚠️ " + implementation.note;
+                }
+            }
+            return feature;
+        });
     }
 }
