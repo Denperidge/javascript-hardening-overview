50a141c6faa28fe84f08706dd167814ddcee1f4f
Denperidge <cat@denperidge.com>
2026-01-07 23:38:25 +0100

    Basic showIf

--- src/pug/config-generator.11tydata.js
+++ src/pug/config-generator.11tydata.js
@@ -16,6 +16,7 @@ export default {
                 const toolIds = Object.keys(implementations);
 
                 let featureInputs = "";
+                const featureShowIf = new Set();
 
                 for (let j=0; j < toolIds.length; j++) {
                     const toolId = toolIds[j];
@@ -25,16 +26,18 @@ export default {
                         const inputType = cfgGen.arg;
                         const dataShowIf = ["project", "global"].map(scope => {
                             if (cfgGen[scope]) {
-                                return `${toolId}-${scope}`
+                                const showIf =`${toolId}-${scope}`;
+                                featureShowIf.add(showIf)
+                                return showIf
                             }
                         });
-                        featureInputs += `<input type="${inputType}"/>`
-
+                        featureInputs += `<input data-show-if="${dataShowIf}" type="${inputType}"/>`
                     }
                 }
 
                 if (featureInputs !== "") {
-                    allInputs += `<fieldset><legend>${feature.title}</legend>${featureInputs}</fieldset>`
+                    const showIf = Array.from(featureShowIf).join(",");
+                    allInputs += `<fieldset data-show-if="${showIf}"><legend>${feature.title}</legend>${featureInputs}</fieldset>`
                 }
             }
 
--- src/static/config-generator.js
+++ src/static/config-generator.js
@@ -5,7 +5,26 @@ function elementsOnClick(querySelector, func) {
         elements[i].addEventListener("click", func);
     }
 }
-elementsOnClick("[name='scope']", (e) => {
-    e.srcElement
-})
+
+function showInputsInScope() {
+    const tool = document.querySelector("[name='tool']:checked").value;
+    const scope = document.querySelector("[name='scope']:checked").value;
+    
+    const showIf = `${tool}-${scope}`;
+
+    const allShowIfElements = document.querySelectorAll(`[data-show-if]`);
+    for (let i = 0; i < allShowIfElements.length; i++) {
+        allShowIfElements[i].setAttribute("style", "opacity: 0.5")
+    }
+
+    const showIfElements = document.querySelectorAll(`[data-show-if*="${showIf}"]`);;
+    console.log(showIfElements)
+    for (let i = 0;  i < showIfElements.length; i++) {
+        showIfElements[i].setAttribute("style", "opacity: 1")
+
+    }
+
+}
+
+elementsOnClick("#general-settings input", showInputsInScope);
 
